d590c0d643fe0c9f5a9bc376eb7db1a3e6e69709
lmoss <larrymoss@gmail.com>
2025-01-01 13:05:19 -0500

    changes on January 1, 2025

--- conf.py
+++ conf.py
@@ -8,43 +8,27 @@ bibtex_bibfiles = ['references.bib']
 comments_config = {'hypothesis': False, 'utterances': False}
 copyright = '2023'
 exclude_patterns = ['**.ipynb_checkpoints', '.DS_Store', 'Thumbs.db', '_build']
-execution_allow_errors = False
-execution_excludepatterns = []
-execution_in_temp = False
-execution_timeout = 30
-extensions = ['sphinx_togglebutton', 'sphinx_copybutton', 'myst_nb', 'jupyter_book', 'sphinx_thebe', 'sphinx_comments', 'sphinx_external_toc', 'sphinx.ext.intersphinx', 'sphinx_design', 'sphinx_book_theme', 'sphinx_exercise', 'sphinxcontrib.bibtex', 'sphinx_jupyterbook_latex', 'sphinx-proof']
+extensions = ['sphinx_togglebutton', 'sphinx_copybutton', 'myst_nb', 'jupyter_book', 'sphinx_thebe', 'sphinx_comments', 'sphinx_external_toc', 'sphinx.ext.intersphinx', 'sphinx_design', 'sphinx_book_theme', 'sphinx_exercise', 'sphinx_proof', 'sphinxcontrib.bibtex', 'sphinx_jupyterbook_latex']
 external_toc_exclude_missing = False
 external_toc_path = '_toc.yml'
 html_baseurl = ''
 html_favicon = ''
-html_logo = 'logo.png'
+html_logo = 'beaver.jpg'
 html_sourcelink_suffix = ''
 html_theme = 'sphinx_book_theme'
-
-html_theme_options = {'search_bar_text': 'Search this book...', 'launch_buttons': {'notebook_interface': 'classic', 'binderhub_url': 
-'https://mybinder.org', 'jupyterhub_url': '', 'thebe': False, 'colab_url': ''}, 'path_to_docs': 'docs', 'repository_url': 
-'https://github.com/lmoss/onesharp', 'repository_branch': 'main', 'google_analytics_id': '', 'extra_navbar': 'Powered by <a 
-href="https://jupyterbook.org">Jupyter Book</a>', 'extra_footer': '', 'home_page_in_toc': True, 'announcement': '', 
-'use_repository_button': True, 'use_edit_page_button': False, 'use_issues_button': True, "show_toc_level": 1,
-"show_navbar_depth":1
-}
-
-
-
-html_title = 'Invitation to Computabitlity'
-jupyter_cache = ''
-jupyter_execute_notebooks = 'off'
-latex_engine = 'xelatex'
-# was pdflatex
-latex_elements = {
-'preamble': r'''
-\input{latex_macros.sty.txt},
-\usepackage{tikz}
-\usetikzlibrary{arrows,shapes,snakes,automata,backgrounds,petri}
-'''
-}
-myst_enable_extensions = ['colon_fence', 'dollarmath', 'linkify', 'substitution', 'tasklist','proof']
+html_theme_options = {'search_bar_text': 'Search this book...', 'launch_buttons': {'notebook_interface': 'classic', 'binderhub_url': '', 'jupyterhub_url': '', 'thebe': False, 'colab_url': ''}, 'path_to_docs': 'docs', 'repository_url': 'https://github.com/lmoss/onesharp', 'repository_branch': 'main', 'extra_footer': '', 'home_page_in_toc': True, 'announcement': '', 'analytics': {'google_analytics_id': ''}, 'use_repository_button': True, 'use_edit_page_button': False, 'use_issues_button': True}
+html_title = 'Invitation to Computability'
+latex_engine = 'pdflatex'
+mathjax3_config = {'tex': {'macros': {'quadiff': ['\\quad\\mbox{iff}\\quad'], 'quadeq': ['\\quad =\\quad'], 'dar': ['\\downarrow'], 'uar': ['\\uparrow'], 'eps': ['\\varepsilon'], 'id': ['id'], 'o': ['\\circ'], 'N': '\\mathbb{N}', 'set': ['\\left\\{#1\\right\\}', 1], 'one': ['\\mathtt{1}'], 'hash': ['\\mathtt{\\#}'], 'diag': ['\\mathtt{diag}'], 'self': ['\\mathtt{self}'], 'copyprog': ['\\mathtt{copy}'], 'clearprog': ['\\mathtt{clear}'], 'writeprog': ['\\mathtt{write}'], 'writetotwo': ['\\mathtt{write}_2'], 'semantics': ['[\\![#1]\\!]', 1], 'semanticsalt': ['\\langle\\!\\langle #1\\rangle\\!\\rangle', 1], 'moveprog': ['\\mathtt{move}'], 'moveprogtwoone': ['\\mathtt{move}_{2,1}'], 'phifn': ['\\varphi'], 'Tile': ['\\mathit{Tile}'], 'TT': ['\\mathcal{T}'], 'DD': ['\\mathcal{D}'], 'Rone': ['R1'], 'Rtwo': ['R2'], 'words': ['\\mbox{words}']}}}
+mathjax_path = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'
+myst_enable_extensions = ['dollarmath', 'amsmath', 'colon_fence', 'deflist']
 myst_url_schemes = ['mailto', 'http', 'https']
+nb_execution_allow_errors = False
+nb_execution_cache_path = ''
+nb_execution_excludepatterns = []
+nb_execution_in_temp = False
+nb_execution_mode = 'off'
+nb_execution_timeout = 30
 nb_output_stderr = 'show'
 numfig = True
 pygments_style = 'sphinx'
